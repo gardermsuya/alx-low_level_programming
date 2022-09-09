ccf1f25251bf9cc5e0805a346565b01f515a70ca
Garder <hildagardrd0812@gmail.com>
2022-09-09 14:49:00 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
-
-
-/*
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+/**
+ * main-Entry point
+ * description: using the main function
+ * Return: 0
+ */
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
