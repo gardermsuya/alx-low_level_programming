4d182ac9baff05c3af2951f0c0139589cd62a087
Garder <hildagardrd0812@gmail.com>
2022-09-09 16:42:46 -0700

    changes

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,15 +8,13 @@
  */
 int main(void)
 {
-	char low, e, q;
+	char low;
 
-	e = 'e';
-	q = 'q';
 	for (low = 'a'; low <= 'z'; low++)
-	{
-		if (low != e && low != q)
 		putchar(low);
-	}
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
 	putchar('\n');
 	return (0);
+
 }
